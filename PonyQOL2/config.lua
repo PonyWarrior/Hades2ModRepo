4cc9dc0ebebe0d3758fbc669765e111e414381c1
PonyWarrior <thibault.soleil@outlook.fr>
2024-05-09 21:06:55 +0200

    PonyQOL2 : new mod

---
+++ PonyQOL2/config.lua
@@ -0,0 +1,11 @@
+local mod = ModUtil.Mod.Register("PQOL2")
+
+mod.Config = {
+	Enabled = true,
+	AllUnlockedToolsUsable = {
+		Enabled = true
+	},
+	AlwaysEncounterStoryRooms = {
+		Enabled = true
+	}
+}
---
+++ PonyQOL2/main.lua
@@ -0,0 +1,31 @@
+local mod = PQOL2
+
+if not mod.Config.Enabled then return end
+
+if mod.Config.AllUnlockedToolsUsable.Enabled then
+	ModUtil.Path.Override("HasAccessToTool", function(toolName)
+		print("DEBUG " .. toolName)
+		if GameState.WorldUpgrades[toolName] then
+			return true
+		end
+		if HasFamiliarTool(toolName) then
+			return true
+		end
+
+		return false
+	end)
+end
+
+if mod.Config.AlwaysEncounterStoryRooms.Enabled then
+	RoomSetData.F.F_Story01.ForceAtBiomeDepthMin = 1
+	RoomSetData.F.F_Story01.ForceAtBiomeDepthMax = 8
+
+	RoomSetData.G.G_Story01.ForceAtBiomeDepthMin = 1
+	RoomSetData.G.G_Story01.ForceAtBiomeDepthMax = 6
+
+	RoomSetData.N.N_Story01.ForceAtBiomeDepthMin = 0
+	RoomSetData.N.N_Story01.ForceAtBiomeDepthMax = 1
+
+	RoomSetData.O.O_Story01.ForceAtBiomeDepthMin = 1
+	RoomSetData.O.O_Story01.ForceAtBiomeDepthMax = 5
+end
